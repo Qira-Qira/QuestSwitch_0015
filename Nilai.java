341b4ff80764655c32b50326597bce3f24e429ba
Qira-Qira <nawafariq3@gmail.com>
2025-11-06 01:08:46 +0700

    Revisi validasi rentang nilai mata kuliah

--- Nilai.java
+++ Nilai.java
@@ -4,8 +4,7 @@ public class Nilai {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        double strukturD,pbo,basisD,algoritma,rpl;
-
+        double strukturD, pbo, basisD, algoritma, rpl;
 
         System.out.print("Masukkan Nilai Struktur Data: ");
         strukturD = sc.nextDouble();
@@ -18,88 +17,89 @@ public class Nilai {
         System.out.print("Masukkan Nilai Rekayasa Perangkat: ");
         rpl = sc.nextDouble();
 
+        if (strukturD < 0 || strukturD > 100 || pbo < 0 || pbo > 100 || basisD < 0 || basisD > 100 || algoritma < 0
+                || algoritma > 100 || rpl < 0 || rpl > 100) {
+            System.out.println("Nilai tidak Valid");
+        } else {
+            double rataRata = (strukturD + pbo + basisD + algoritma + rpl) / 5;
+            double ipk = (rataRata / 100) * 4;
 
-        double rataRata = (strukturD + pbo + basisD + algoritma + rpl) / 5;
-        double ipk = (rataRata / 100) * 4;
+            String predikat;
 
-
-        String predikat;
-        
-        if(ipk < 0 || ipk > 4.00){
-           predikat = "Masukkan nilai matkul dengan benar";
-        }else if(ipk >= 3.75){
-            predikat = "A (Sangat Baik)";
-        }else if(ipk >= 3.50){
-            predikat = "AB (Baik Sekali)";
-        }else if(ipk >= 3.00){
-            predikat = "B (Baik)";
-        }else if(ipk >= 2.50){
-            predikat = "BC (Cukup)";
-        }else{
-            predikat = "C (Kurang)";
-        }
-
-        System.out.println("\nHasil Nilai\n");
-        System.out.println("IPK: " + ipk );
-        System.out.println("Predikat: " + predikat);
-
-        if(predikat.equals("A (Sangat Baik)") || predikat.equals("AB (Baik Sekali)")){
-            System.out.println("\nDaftar Mata Kuliah Yang Dapat Diajukan Untuk Posisi Asisten: \n");
-            System.out.println("1. Struktur Data");
-            System.out.println("2. Pemrograman Berorientasi Objek");
-            System.out.println("3. Basis Data");
-            System.out.println("4. Algoritma dan Pemrograman");
-            System.out.println("5. Rekayasa Perangkat Lunak");
-
-            System.out.print("\nPilih Mata Kuliah Yang memiliki nilai minimal 75: ");
-            int pilihan = sc.nextInt();
-
-            switch(pilihan){
-                case 1:
-                    if(strukturD >= 75){
-                        System.out.println("Selamat, Mahasiswa eligible untuk menjadi Asisten Dosen");
-                    }else{
-                        System.out.println("Mahasiswa Belum memenuhi syarat untuk posisi tersebut");
-                    }
-                    break;
-                case 2:
-                    if(pbo >= 75){
-                        System.out.println("Selamat, Mahasiswa eligible untuk menjadi Asisten Dosen");
-                    }else{
-                        System.out.println("Mahasiswa Belum memenuhi syarat untuk posisi tersebut");
-                    }
-                    break;
-                case 3:
-                    if(basisD >= 75){
-                        System.out.println("Selamat, Mahasiswa eligible untuk menjadi Asisten Dosen");
-                    }else{
-                        System.out.println("Mahasiswa Belum memenuhi syarat untuk posisi tersebut");
-                    }
-                    break;
-                case 4:
-                    if(algoritma >= 75){
-                        System.out.println("Selamat, Mahasiswa eligible untuk menjadi Asisten Dosen");
-                    }else{
-                        System.out.println("Mahasiswa Belum memenuhi syarat untuk posisi tersebut");
-                    }
-                    break;
-                case 5:
-                    if(rpl >= 75){
-                        System.out.println("Selamat, Mahasiswa eligible untuk menjadi Asisten Dosen");
-                    }else{
-                        System.out.println("Mahasiswa Belum memenuhi syarat untuk posisi tersebut");
-                    }
-                    break;
-
-                default:
-                    System.out.println("Pilih Mata Kuliah Mulai dari 1 hingga 5");
-                    break;
+            if (ipk >= 3.75) {
+                predikat = "A (Sangat Baik)";
+            } else if (ipk >= 3.50) {
+                predikat = "AB (Baik Sekali)";
+            } else if (ipk >= 3.00) {
+                predikat = "B (Baik)";
+            } else if (ipk >= 2.50) {
+                predikat = "BC (Cukup)";
+            } else {
+                predikat = "C (Kurang)";
             }
-        }else{
-            System.out.println("\nMahasiswa Belum Memenuhi  Syarat");
-        }
 
+            System.out.println("\nHasil Nilai\n");
+            System.out.println("IPK: " + ipk);
+            System.out.println("Predikat: " + predikat);
+
+            if (predikat.equals("A (Sangat Baik)") || predikat.equals("AB (Baik Sekali)")) {
+                System.out.println("\nDaftar Mata Kuliah Yang Dapat Diajukan Untuk Posisi Asisten: \n");
+                System.out.println("1. Struktur Data");
+                System.out.println("2. Pemrograman Berorientasi Objek");
+                System.out.println("3. Basis Data");
+                System.out.println("4. Algoritma dan Pemrograman");
+                System.out.println("5. Rekayasa Perangkat Lunak");
+
+                System.out.print("\nPilih Mata Kuliah Yang memiliki nilai minimal 75: ");
+                int pilihan = sc.nextInt();
+
+                switch (pilihan) {
+                    case 1:
+                        if (strukturD >= 75) {
+                            System.out.println("Selamat, Mahasiswa eligible untuk menjadi Asisten Dosen");
+                        } else {
+                            System.out.println("Mahasiswa Belum memenuhi syarat untuk posisi tersebut");
+                        }
+                        break;
+                    case 2:
+                        if (pbo >= 75) {
+                            System.out.println("Selamat, Mahasiswa eligible untuk menjadi Asisten Dosen");
+                        } else {
+                            System.out.println("Mahasiswa Belum memenuhi syarat untuk posisi tersebut");
+                        }
+                        break;
+                    case 3:
+                        if (basisD >= 75) {
+                            System.out.println("Selamat, Mahasiswa eligible untuk menjadi Asisten Dosen");
+                        } else {
+                            System.out.println("Mahasiswa Belum memenuhi syarat untuk posisi tersebut");
+                        }
+                        break;
+                    case 4:
+                        if (algoritma >= 75) {
+                            System.out.println("Selamat, Mahasiswa eligible untuk menjadi Asisten Dosen");
+                        } else {
+                            System.out.println("Mahasiswa Belum memenuhi syarat untuk posisi tersebut");
+                        }
+                        break;
+                    case 5:
+                        if (rpl >= 75) {
+                            System.out.println("Selamat, Mahasiswa eligible untuk menjadi Asisten Dosen");
+                        } else {
+                            System.out.println("Mahasiswa Belum memenuhi syarat untuk posisi tersebut");
+                        }
+                        break;
+
+                    default:
+                        System.out.println("Pilih Mata Kuliah Mulai dari 1 hingga 5");
+                        break;
+                }
+            } else {
+                System.out.println("\nMahasiswa Belum Memenuhi  Syarat");
+            }
+
+        }
         sc.close();
-        
+
     }
 }
